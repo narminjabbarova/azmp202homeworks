1f13086cd5193d977eff72afcd5652855fc8c588
narminjabbarova <narminjabbar@gmail.com>
2024-10-30 12:25:33 +0400

    js 1st homework

---
+++ JavaScript-tasks/tasks.js
@@ -0,0 +1,90 @@
+// let maash = prompt("Zehmet olmasa maashinizi daxil edin: ")
+// let mebleg = prompt("Zehmet olmasa istediyiniz meblegi daxil edin: ")
+// let ay = prompt("Zehmet olmasa istediyiniz muddeti(ay) daxil edin: ")
+// if(ay>36){
+//     alert("Size bu shertlerle credit dushmur")
+//     consoleError
+// }
+
+// task1
+// let a = prompt("1-ci kateti daxil edin: ")
+// let b = prompt("2-ci kateti daxil edin: ")
+// let c = prompt("hipotenuzu daxil edin:")
+// if(a**2 + b**2 == Math.sqrt(c)){
+//     alert("Duzbucagli uchbucagdir")
+// }
+// else{
+//     alert("Duzbucagli uchbucag deyil")
+// }
+
+// task2
+// let number = +prompt("Zehmet olmasa 3 reqemli eded daxil edin: ");
+// let number1 = Math.floor(number / 100);
+// let number2 = Math.floor((number % 100 )/ 10);
+// let number3 = Math.floor(number % 10);
+// if(number >= 100 && number <= 999){
+//     alert(`result: ${number1}, ${number2}, ${number3}`)
+// }
+// else{
+//     alert("Zehmet olmasa duzgun 3 reqemli eded daxil edin: ")
+// }
+
+// task3
+// let month = prompt('Enter the month: ')
+// switch (month) {
+//     case January: alert('January has 31 days')
+//         break;
+//         case February: alert('February has 28 days')
+//         break;
+//         case March: alert('March has 31 days')
+//         break;
+//         case April: alert('April has 30 days')
+//         break;
+//         case May: alert('May has 31 days')
+//         break;
+//         case June: alert('June has 30 days')
+//         break;
+//         case July: alert('July has 31 days')
+//         break;
+//         case August: alert('August has 31 days')
+//         break;
+//         case September: alert('September has 30 days')
+//         break;
+//         case October: alert('October has 31 days')
+//         break;
+//         case November: alert('November has 30 days')
+//         break;
+//         case December: alert('December has 31 days')
+//         break;
+
+//     default: alert("enter the correct month")
+//         break;
+// }
+
+// task4
+    // let a = +prompt("Enter the first number:");
+    // let b = +prompt("Enter the second number:");
+    // let c = +prompt("Enter the third number:");
+
+    // let sumAB = a + b;
+    // let sumAC = a + c;
+    // let sumBC = b + c;
+    // let maxSum = Math.max(sumAB, sumAC, sumBC);
+ 
+    // if (maxSum === sumAB) {
+    //     alert(`Big sum result: ${a} and ${b}`);
+    // } else if (maxSum === sumAC) {
+    //     alert(`Big sum result: ${a} and ${c}`);
+    // } else {
+    //     alert(`Big sum result: ${b} and ${c}`);
+    // }
+
+
+
+
+
+
+
+
+
+
