d0027bd951f9102b0254fc029385c9614a625a38
narminjabbarova <narminjabbar@gmail.com>
2024-10-31 12:47:41 +0400

    loops done

---
+++ 01.JS-variables.control-structure.loops/main.js
@@ -0,0 +1,93 @@
+// let number = prompt("Zehmet olmasa, 3 reqemli bir eded daxil edin: ");
+// let sum = (number[1] + number[2] + number[3]);
+// for (let i = 0; i < number.length; i++) {
+//     if(number )
+// }
+
+// 1) Verilmiş 3 rəqəmli ədədin rəqəmlərinin cəmini, hasilini və ədədi ortasını tapan proqram tərtib edin.
+// let number = +prompt("ucreqemli eded daxil edin: ");
+// let digit3 = number % 10;
+// let digit2 = (number % 100 - digit3) / 10;
+// let digit1 = (number - number%100)/100;
+// let sum = digit1 + digit2 + digit3;
+// let multiply = digit1 * digit2 * digit3;
+// let average = sum / 3;
+// alert(`Ededi orta: ${average}, Cem: ${sum}, Hasil: ${multiply}`);
+
+// 2) Verilmiş ədədin bütün bölənlərini console'a çıxaran proqram tərtib edin.
+// let number = +prompt("Eded daxil edin: ");
+// for (let i = 0; i <= number; i++) {
+//     if(number % i == 0){
+//         console.log(i);
+        
+//     }
+    
+// }
+
+// 3) Verilmiş ədədin bölənlərinin sayını tapan proqram tərtib edin.
+
+// let number = +prompt("Eded daxil edin.");
+// let count = 0;
+
+// for (let i = 0; i <= number; i++) {
+//     if (number % i == 0) {
+//         count++
+        
+//     }    
+// }
+
+// alert(count)
+
+// 4) for loop'undan istifadə edərək aşağıdakı console'da aşağıdakı görüntünü alın
+
+
+
+// 
+
+// 6) 0 - 100 arası tək ədədlərin və cüt əd ədlərin cəmini tapın
+// let oddsum = 0
+//  for (let i = 0; i <100; i++) {
+//   if  (i%2 == 1){
+// oddsum++
+
+// }
+// }
+// console.log(oddsum);
+
+// let evensum = 0
+// for (let i = 0; i <= 100; i++) {
+//  if(i%2 == 0){
+//    evensum++
+// }   
+// }
+// console.log(evensum);  
+
+// 7) Arraydəki ən böyük və ən kiçik element-ləri tapan alqoritm yazın.
+// let  array = [4,2,7,9,3,12,46,32,19];
+// let max = array[0];
+// let min = array[0];
+// for (let i = 0; i < array.length; i++) {   
+//     if(array[i]>max){
+//         max = array[i];
+//     } 
+//     else if(array[i]< min){
+//         min = array[i];
+//     }
+// }
+// console.log(max, min);
+
+
+
+// 8)---------
+
+// let sampleNews = `As software developers, the natural tendency is to start developing applications based on your own hands-on experience and knowledge right away. However, overtime issues in the application arise, adaptations to changes, and new features happen.`;
+let count = 0;
+for (let i = 0; i < sampleNews.length; i++) {
+    if (sampleNews[i] == " "){
+        count++
+    }
+
+    
+}
+
+console.log(count);
